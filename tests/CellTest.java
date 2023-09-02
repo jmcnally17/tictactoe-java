0a2b25c37e17343b2300dd0d5b226bd3949fc944
Joe McNally <joe.t.mcnally@hotmail.com>
2023-09-03 00:45:21 +0100

    Set up project with Maven
    Create a Maven pom.xml for the project directory and set it up with customised
    source and test directories and output directories.
    
    Begin testing with some basic JUnit tests for the Cell class.

---
+++ tests/CellTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for the Cell class
+ */
+public class CellTest {
+    private Cell cell;
+
+    @BeforeEach
+    public void setUpCell() {
+        this.cell = new Cell("4");
+    }
+
+    @Test
+    public void isFilledIsFalseUponInstantiation() {
+        Assertions.assertFalse(this.cell.hasBeenFilled());
+    }
+
+    @Test
+    public void valueIsInstantiatedWithParameterInConstructor() {
+        Assertions.assertEquals("4", this.cell.getValue());
+    }
+
+    @Test
+    public void setValueChangesValueProperty() {
+        this.cell.setValue("X");
+        Assertions.assertEquals("X", this.cell.getValue());
+    }
+
+    @Test
+    public void fillSetsValueToXForPlayer1Turn() {
+        this.cell.fill(1);
+        Assertions.assertEquals("X", this.cell.getValue());
+    }
+
+    @Test
+    public void fillSetsValueToOForPlayer2Turn() {
+        this.cell.fill(2);
+        Assertions.assertEquals("O", this.cell.getValue());
+    }
+
+    @Test
+    public void fillSetsIsFilledToTrue() {
+        this.cell.fill(1);
+        Assertions.assertTrue(this.cell.hasBeenFilled());
+    }
+}
